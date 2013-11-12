c38459dbfcc8c385b9b45746c0a938d675d5d43b
Matt Glover <matt@duchysoftware.com>
2013-11-12 21:00:16 +0000

    Initial Commit

---
+++ DuchyBotDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DuchyBotDemo
+//
+//  Created by Matt Glover on 12/11/2013.
+//  Copyright (c) 2013 Duchy Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
